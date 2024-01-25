e2fb1b881f99091a2f9edca7e1c4db926df0c38d
code-wiki <aditya.gautam003@gmail.com>
2024-01-25 18:59:15 +0530

    adding react snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './routers/root';
+import { hydrate, render } from 'react-dom';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+const rootElement: HTMLElement | null = document.getElementById('root');
+let shouldHydrate: boolean = false;
+if (rootElement instanceof HTMLElement) {
+    shouldHydrate = rootElement.hasChildNodes();
+}
+
+if (shouldHydrate) {
+    hydrate(<App />, rootElement);
+} else {
+    render(<App />, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
