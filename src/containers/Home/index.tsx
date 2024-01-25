372185711808581c551c5c82b28bb4897d2f6076
code-wiki <aditya.gautam003@gmail.com>
2024-01-25 17:56:42 +0530

    bug fix

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,8 +1,15 @@
 import { Link } from 'react-router-dom';
+import HelmetComp from '../HelmetComp';
+import IMG from '../../assets/31358195_l.jpg';
 
 export default function index() {
     return (
         <div>
+            <HelmetComp
+                title={'Sparta app'}
+                description={'Sparta app: new generation app'}
+                imageUrl={IMG}
+            />
             <div>This is home section</div>
             <Link to={'/blog'} className="">
                 {'go to Blog ->'}
--- src/routers/root.tsx
+++ src/routers/root.tsx
@@ -1,21 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../containers/Home';
-import HelmetComp from '../containers/HelmetComp';
-import IMG from '../assets/31358195_l.jpg';
 import Blog from '../containers/Blog';
+import { HelmetProvider } from 'react-helmet-async';
 
 export default function root() {
     return (
         <BrowserRouter>
-            <HelmetComp
-                title={'Sparta app'}
-                description={'Sparta app: new generation app'}
-                imageUrl={IMG}
-            />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/blog" element={<Blog />} />
-            </Routes>
+            <HelmetProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog" element={<Blog />} />
+                </Routes>
+            </HelmetProvider>
         </BrowserRouter>
     );
 }
